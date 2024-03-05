89bcac8a6824df949df07511248390d4e471d6c1
africam <africamatji@gmail.com>
2024-03-05 09:12:43 +0200

    initial commit, lets build our dreams

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function find()
+    {
+
+    }
+
+    public function all()
+    {
+
+    }
+
+    public function getById()
+    {
+
+    }
+}
