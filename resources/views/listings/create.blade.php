6bd5bc4a1483cad4d858c5cf81a6690196f306d1
africam <africamatji@gmail.com>
2024-03-10 19:25:30 +0200

    improve UI

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -44,7 +44,7 @@
         </div>
 
         <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Create Listing
+            Create
         </button>
     </form>
 @endsection
